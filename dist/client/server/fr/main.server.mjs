446623fbca0902937fc7b3193c776febe0c4ed31
Faima Saad Naik <fatima.saadn02@gmail.com>
2024-07-10 18:31:45 +0500

    Revert "some commit"
    This reverts commit 826ca3e913ef8cefa9c94e50ced858965c21bf60.

--- dist/client/server/fr/main.server.mjs
+++
@@ -1,3 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-EPNSBAKU.mjs";import"./chunk-MYEMF3ZT.mjs";import"./chunk-7W4EFSXQ.mjs";import"./chunk-FJFEIO2B.mjs";import"./chunk-63Y3PX24.mjs";export{a as default};
-/**i18n:b4befb39f552161274a3bff6194322ed1709f00556e578d79f451384a64ff146*/
